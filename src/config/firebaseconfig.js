a288541180d55fe3ed84c7e059799748198af67a
kevin <contactohuayta@gmail.com>
2026-02-12 07:50:39 -0300

    se agregaron variables de entorno para trabajar con una version staging/develop

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB1jt4I1rde30Bu7xh65ypW--UQE0iWAdk",
-  authDomain: "affinityteach.firebaseapp.com",
-  projectId: "affinityteach",
-  storageBucket: "affinityteach.firebasestorage.app",
-  messagingSenderId: "42045134258",
-  appId: "1:42045134258:web:158658dbfe02d0775000ab",
-  measurementId: "G-G7J1DG6JVZ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
